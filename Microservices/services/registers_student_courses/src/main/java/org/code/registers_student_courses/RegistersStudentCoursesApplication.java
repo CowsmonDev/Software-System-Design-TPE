dc64d1f578eb97c07391e983de130b1796154a13
Agustin Tomas Crespo <agustin.crespo22@gmail.com>
2024-11-11 16:42:42 -0300

    Register Course Student Configuracion Inicial

---
+++ Microservices/services/registers_student_courses/src/main/java/org/code/registers_student_courses/RegistersStudentCoursesApplication.java
@@ -0,0 +1,13 @@
+package org.code.registers_student_courses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistersStudentCoursesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistersStudentCoursesApplication.class, args);
+	}
+
+}
